a348a4b8584d2401ba125e84c28d59ec0ed6f482
Clyde JANNEL <clyde.jannel@univ-rennes1.fr>
2022-10-07 15:09:13 +0200

    Add Implementation of the Selection interface

---
+++ src/fr/istic/aco/editor/SelectionImpl.java
@@ -0,0 +1,85 @@
+package fr.istic.aco.editor;
+
+public class SelectionImpl implements Selection {
+    private int beginIndex;
+    private int endIndex;
+    private final int bufferBeginIndex = 0;
+    private int bufferEndIndex;
+
+    public SelectionImpl(int beginIndex, int endIndex, StringBuffer buffer) {
+        this.beginIndex = beginIndex;
+        this.endIndex = endIndex;
+        this.bufferEndIndex = buffer.length()-1;
+    }
+    /**
+     * Provides the index of the first character designated
+     * by the selection.
+     *
+     * @return the beginning index
+     */
+    @Override
+    public int getBeginIndex() {
+        return this.beginIndex;
+    }
+
+    /**
+     * Provides the index of the first character
+     * after the last character designated
+     * by the selection.
+     *
+     * @return the end index
+     */
+    @Override
+    public int getEndIndex() {
+        return this.endIndex;
+    }
+
+    /**
+     * Provides the index of the first character in the buffer
+     *
+     * @return the buffer's begin index
+     */
+    @Override
+    public int getBufferBeginIndex() {
+        return this.bufferBeginIndex;
+    }
+
+    /**
+     * Provides the index of the first "virtual" character
+     * after the end of the buffer
+     *
+     * @return the post end buffer index
+     */
+    @Override
+    public int getBufferEndIndex() {
+        return this.bufferEndIndex;
+    }
+
+    /**
+     * Changes the value of the start index of the selection
+     *
+     * @param beginIndex@throws IndexOutOfBoundsException if the beginIndex is out of bounds
+     */
+    @Override
+    public void setBeginIndex(int beginIndex) {
+        if(beginIndex < getBufferBeginIndex()){
+            throw new IndexOutOfBoundsException();
+        } else {
+        this.beginIndex = beginIndex;
+        }
+    }
+
+    /**
+     * Changes the value of the end index of the selection
+     *
+     * @param endIndex@throws IndexOutOfBoundsException if the beginIndex is out of bounds
+     */
+    @Override
+    public void setEndIndex(int endIndex) {
+        if(endIndex > getBufferEndIndex()){
+            throw new IndexOutOfBoundsException();
+        } else {
+            this.endIndex = endIndex;
+        }
+    }
+}
