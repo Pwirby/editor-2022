50041f0221295fef12baf823047119608ffc3ad9
Clyde JANNEL <clyde.jannel@univ-rennes1.fr>
2022-10-17 19:10:07 +0200

    Fixed the way selections are implemented in tests

--- src/fr/istic/aco/editor/EngineTest.java
+++ src/fr/istic/aco/editor/EngineTest.java
@@ -8,10 +8,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class EngineTest {
     String string1 = "the quick brown fox jumps over the lazy dog";
     private Engine engine;
+    private Selection selection;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         engine = new EngineImpl();
+        selection = engine.getSelection();
     }
 
     private void todo() {
@@ -22,68 +24,97 @@ class EngineTest {
     @DisplayName("Buffer must be empty after initialisation")
     void getSelection() {
         //|><|
-        Selection selection = engine.getSelection();
+        selection = engine.getSelection();
         assertEquals(selection.getBufferBeginIndex(), selection.getBeginIndex());
         assertEquals("", engine.getBufferContents());
+        assertEquals(0, selection.getBeginIndex());
+        assertEquals(0, selection.getEndIndex());
     }
 
     @Test
     void getBufferContents() {
         //the quick brown fox jumps over the lazy dog|><|
         engine.insert(string1);
+        selection = engine.getSelection();
+
         assertEquals(string1, engine.getBufferContents());
+        assertEquals(string1.length(), selection.getBeginIndex());
+        assertEquals(string1.length(), selection.getEndIndex());
     }
 
     @Test
     void getClipboardContents() {
         //the quick brown fox jumps over the lazy dog|><|
         engine.insert(string1);
-        Selection selection = engine.getSelection();
+
         //|>the quick brown fox jumps over the lazy dog<|
+        selection = engine.getSelection();
         selection.setBeginIndex(engine.getSelection().getBufferBeginIndex());
         engine.copySelectedText();
+
         assertEquals(string1, engine.getClipboardContents());
         assertEquals(string1, engine.getBufferContents());
+        assertEquals(0, selection.getBeginIndex());
+        assertEquals(string1.length(), selection.getEndIndex());
     }
 
     @Test
+    @DisplayName("cutSelectedText")
     void cutSelectedText() {
         //the quick brown fox jumps over the lazy dog|><|
         engine.insert(string1);
-        Selection selection = engine.getSelection();
+
         //|>the quick brown fox jumps over the lazy dog<|
+        selection = engine.getSelection();
         selection.setBeginIndex(engine.getSelection().getBufferBeginIndex());
+
         //|><|
         engine.cutSelectedText();
+        selection = engine.getSelection();
+
         assertEquals(string1, engine.getClipboardContents());
         assertEquals("", engine.getBufferContents());
+        assertEquals(0, selection.getBeginIndex());
+        assertEquals(0, selection.getEndIndex());
     }
 
     @Test
     void copySelectedText() {
         //the quick brown fox jumps over the lazy dog|><|
         engine.insert(string1);
-        Selection selection = engine.getSelection();
+
         //|>the quick brown fox jumps over the lazy dog<|
+        selection = engine.getSelection();
         selection.setBeginIndex(engine.getSelection().getBufferBeginIndex());
         engine.copySelectedText();
+
         assertEquals(string1, engine.getClipboardContents());
         assertEquals(string1, engine.getBufferContents());
+        assertEquals(0, selection.getBeginIndex());
+        assertEquals(string1.length(), selection.getEndIndex());
+
     }
 
     @Test
     void pasteClipboard() {
         //the quick brown fox jumps over the lazy dog|><|
         engine.insert(string1);
-        Selection selection = engine.getSelection();
+
         //|>the quick brown fox jumps over the lazy dog<|
+        Selection selection = engine.getSelection();
         selection.setBeginIndex(engine.getSelection().getBufferBeginIndex());
         engine.copySelectedText();
+
         //|><|the quick brown fox jumps over the lazy dog
         selection.setEndIndex(0);
-        //|>the quick brown fox jumps over the lazy dog<|the quick brown fox jumps over the lazy dog
+
+        //the quick brown fox jumps over the lazy dog|><|the quick brown fox jumps over the lazy dog
         engine.pasteClipboard();
+
+        selection = engine.getSelection();
         assertEquals(string1, engine.getClipboardContents());
-        assertEquals(string1+string1, engine.getBufferContents());
+        assertEquals(string1 + string1, engine.getBufferContents());
+        assertEquals(string1.length(), selection.getBeginIndex());
+        assertEquals(string1.length(), selection.getEndIndex());
     }
 }
