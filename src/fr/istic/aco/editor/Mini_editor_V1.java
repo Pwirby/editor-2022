3c53e5a058bad35155ceb3fe065f1d2e1e8126ab
Theophile Carrasco <theophile.carrasco@etu.univ-poitiers.fr>
2022-10-25 14:19:51 +0200

    Add Mini_Editor_V1

---
+++ src/fr/istic/aco/editor/Mini_editor_V1.java
@@ -0,0 +1,32 @@
+package fr.istic.aco.editor;
+
+public class Mini_editor_V1{
+
+    private Engine engine;
+    private UserInterface userInterface;
+
+    public static void main(String[] args){
+        Mini_editor_V1 miniEditor = new Mini_editor_V1();
+        miniEditor.run();
+    }
+
+
+    private void run(){
+
+        engine = new EngineImpl();
+        userInterface = new UserInterfaceImpl();
+
+        userInterface.setReadStream(System.in);
+
+        configureCommands();
+
+        userInterface.runInvokerLoop();
+    }
+
+    public void configureCommands(){
+        userInterface.addCommand("Copy", new CopyCommand(engine));
+        userInterface.addCommand("Cut", new CutCommand(engine));
+        userInterface.addCommand("Paste", new PasteCommand(engine));
+    }
+
+}
