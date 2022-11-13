a7dd6675f3a4cbc535c1998000e7a16673ff5a57
Clyde JANNEL <clyde.jannel@univ-rennes1.fr>
2022-11-13 12:11:12 +0100

    Add test verifying IndexOutOfBounds & better explain test scenarios

--- src/fr/istic/aco/editor/EngineTest.java
+++ src/fr/istic/aco/editor/EngineTest.java
@@ -32,87 +32,111 @@ class EngineTest {
     }
 
     @Test
+    @DisplayName("Selector bounds should be placed after the insertion")
     void getBufferContents() {
         //the quick brown fox jumps over the lazy dog|><|
         engine.insert(string1);
         selection = engine.getSelection();
 
         assertEquals(string1, engine.getBufferContents());
-        assertEquals(string1.length(), selection.getEndIndex());
+        assertEquals(string1.length(), selection.getBeginIndex(), selection.getEndIndex());
     }
 
     @Test
+    @DisplayName("Clipboard should contain last copied/cut text")
     void getClipboardContents() {
         //the quick brown fox jumps over the lazy dog|><|
         engine.insert(string1);
 
         //|>the quick brown fox jumps over the lazy dog<|
         selection = engine.getSelection();
-        selection.setBeginIndex(engine.getSelection().getBufferBeginIndex());
+        selection.setBeginIndex(selection.getBufferBeginIndex());
         engine.copySelectedText();
 
         assertEquals(string1, engine.getClipboardContents());
         assertEquals(string1, engine.getBufferContents());
-        assertEquals(0, selection.getBeginIndex());
+        assertEquals(selection.getBufferBeginIndex(), selection.getBeginIndex());
         assertEquals(string1.length(), selection.getEndIndex());
     }
 
     @Test
-    @DisplayName("cutSelectedText")
+    @DisplayName("Cut text should be stored in clipboard and selector bounds placed where the text was")
     void cutSelectedText() {
         //the quick brown fox jumps over the lazy dog|><|
         engine.insert(string1);
 
         //|>the quick brown fox jumps over the lazy dog<|
         selection = engine.getSelection();
-        selection.setBeginIndex(engine.getSelection().getBufferBeginIndex());
+        selection.setBeginIndex(selection.getBufferBeginIndex());
 
         //|><|
         engine.cutSelectedText();
-        selection = engine.getSelection();
 
         assertEquals(string1, engine.getClipboardContents());
         assertEquals("", engine.getBufferContents());
-        assertEquals(0, selection.getBeginIndex());
-        assertEquals(0, selection.getEndIndex());
+        assertEquals(selection.getBufferBeginIndex(), selection.getBeginIndex());
+        assertEquals(selection.getBufferBeginIndex(), selection.getEndIndex());
     }
 
     @Test
+    @DisplayName("Copied text should be stored in clipboard and selection indexes unchanged")
     void copySelectedText() {
         //the quick brown fox jumps over the lazy dog|><|
         engine.insert(string1);
 
         //|>the quick brown fox jumps over the lazy dog<|
         selection = engine.getSelection();
-        selection.setBeginIndex(engine.getSelection().getBufferBeginIndex());
+        selection.setBeginIndex(selection.getBufferBeginIndex());
         engine.copySelectedText();
 
         assertEquals(string1, engine.getClipboardContents());
         assertEquals(string1, engine.getBufferContents());
-        assertEquals(0, selection.getBeginIndex());
+        assertEquals(selection.getBufferBeginIndex(), selection.getBeginIndex());
         assertEquals(string1.length(), selection.getEndIndex());
 
     }
 
     @Test
+    @DisplayName("Copy/pasting should result in the clipboard still containing the copy, and the buffer twice that text")
     void pasteClipboard() {
         //the quick brown fox jumps over the lazy dog|><|
         engine.insert(string1);
 
         //|>the quick brown fox jumps over the lazy dog<|
         Selection selection = engine.getSelection();
-        selection.setBeginIndex(engine.getSelection().getBufferBeginIndex());
+        selection.setBeginIndex(selection.getBufferBeginIndex());
         engine.copySelectedText();
 
         //|><|the quick brown fox jumps over the lazy dog
-        selection.setEndIndex(0);
+        selection.setEndIndex(selection.getBufferBeginIndex());
 
         //the quick brown fox jumps over the lazy dog|><|the quick brown fox jumps over the lazy dog
         engine.pasteClipboard();
 
-        selection = engine.getSelection();
         assertEquals(string1, engine.getClipboardContents());
-        assertEquals(string1 + string1, engine.getBufferContents());
-        assertEquals(string1.length(), selection.getEndIndex());
+        assertEquals(string1.concat(string1), engine.getBufferContents());
+        assertEquals(string1.length(), selection.getBeginIndex(), selection.getEndIndex());
+    }
+
+    @Test
+    @DisplayName("Setting the begin index before the buffer begin index throws an error")
+    void setBeginIndex() {
+        //the quick brown fox jumps over the lazy dog|><|
+        engine.insert(string1);
+
+        //|><- the quick brown fox jumps over the lazy dog<|
+        Selection selection = engine.getSelection();
+        assertThrows(IndexOutOfBoundsException.class, () -> selection.setBeginIndex(selection.getBufferBeginIndex() - 1));
+    }
+
+    @Test
+    @DisplayName("Setting the begin index before the buffer begin index throws an error")
+    void setEndIndex() {
+        //the quick brown fox jumps over the lazy dog|><|
+        engine.insert(string1);
+
+        //the quick brown fox jumps over the lazy dog|> -><|
+        Selection selection = engine.getSelection();
+        assertThrows(IndexOutOfBoundsException.class, () -> selection.setEndIndex(selection.getBufferEndIndex() + 1));
     }
 }
