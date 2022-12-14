fde65a0b7023f0e491454ac813dfe044fa6f05d6
Clyde JANNEL <clyde.jannel@univ-rennes1.fr>
2022-12-14 17:30:17 +0100

    Makes implement memento to PasteCommand

--- src/fr/istic/aco/editor/Recorder.java
+++ src/fr/istic/aco/editor/Recorder.java
@@ -3,11 +3,24 @@ package fr.istic.aco.editor;
 import fr.istic.aco.editor.commands.Command;
 
 public interface Recorder {
-
+    /**
+     * Save the command in the recorder.
+     * @param c the command to save
+     */
     void saveCommand(Command c);
     // Start recording at the beginning ?
+    /**
+     * Start the recording of every executed command.
+     */
     void start();
+    /**
+     * Stop the recording of every executed command.
+     */
     void stop();
+
+    /**
+     * Re-execute every command saved.
+     */
     void replay();
 
 }
